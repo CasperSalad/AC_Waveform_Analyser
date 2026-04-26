7e0ace0c2a2e97655d16661804eeab44f6d2d01e
CasperSalad <Casper2.Lamptey@live.uwe.ac.uk>
2026-04-26 17:05:53 +0100

    added the csv file to git

--- IO.h
+++ IO.h
@@ -1,8 +1,8 @@
 //
 // Created by clt2-lamptey on 21/04/2026.
 //
-
+#include "waveform.h"
 #ifndef WAVEFORM_ANALYSER_CODE_IO_H
 #define WAVEFORM_ANALYSER_CODE_IO_H
-
+Sample* COLLECT(char *CSVFILE);
 #endif //WAVEFORM_ANALYSER_CODE_IO_H
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
  #include <stdio.h>
 #include <math.h>
+#include "IO.h"
+#include "waveform.h"
 int main() {
 
     return 0;
